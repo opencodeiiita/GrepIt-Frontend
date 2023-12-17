76bd2d93dbbbc29ca2b364ad5d9f5804df093638
Wadad Parker <wadadparker5@gmail.com>
2023-12-17 15:31:27 +0530

    feat: updated styles

--- src/pages/CreateRoom.jsx
+++ src/pages/CreateRoom.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import RoomLink from "../components/RoomLink";
 
 const CreateRoom = () => {
+  const [showModal,setShowModal] = useState(false);
   return (
-    <div>CreateRoom
-      <RoomLink roomLink={"https://x.com/wadadparker"}/>
+    <div>
+      <button className='bg-black text-white p-2' onClick={()=>setShowModal(true)}>Click to open room link modal</button>
+      {showModal && <RoomLink roomLink={"https://x.com/wadadparker"} setShowModal={setShowModal}/>}
     </div>
   )
 }
