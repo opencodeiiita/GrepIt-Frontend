0158c4b795656fe57c7f9a9db91982b5d601001f
Wadad Parker <wadadparker5@gmail.com>
2023-12-22 01:49:53 +0530

    feat: logic to add new question on frontend

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -17,21 +17,45 @@ const Option=({optionValue,optionLabel,placeholder, correctOption, changeCorrect
 
 const Quiz = () => {
   const [question,setQuestion] = useState("");
-  const [options,setOptions] = useState({option1:"",option2:"",option3:"",option4:""})
+  const [options,setOptions] = useState({ 1: "", 2: "", 3: "", 4: "" })
   const [correctOption,setCorrectOption] = useState("");
+
+  // Maintain this below state in redux
+  const [questions, setQuestions]= useState([]);
 
   const changeCorrectOption = (selectedOption) => selectedOption!==correctOption?setCorrectOption(selectedOption):setCorrectOption("");
 
+  const clickHandler=()=>
+  {
+    // Add the actual function to call the API here, fetch the main state from redux
+    const finalQuestion = {
+      question: question,
+      options: Object.keys(options).map((key) => ({
+        option: options[key],
+        isCorrect: (correctOption === key)
+      }))
+    };
+    setQuestions(prev=>[...prev,finalQuestion]);
+    
+  }
+
   return (
-    <div className='w-screen h-screen flex flex-col justify-start items-center'>
-      <section className='w w-2/5 bg-gray-200/70 py-3 mt-1 px-2.5 rounded-xl flex flex-col h-1/4 shadow-sm shadow-gray-600'>
-        <textarea className='w-full grow  text-center placeholder:text-black bg-white/80 rounded-lg resize-none pt-[5%]' placeholder='question goes here...' />
-        <main className='grid grid-cols-2 justify-center items-center gap-y-2 gap-x-4 mx-2 mt-3'>
-          <Option optionValue={""} optionLabel={"option1"} placeholder={"1st Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
-          <Option optionValue={""} optionLabel={"option2"} placeholder={"2nd Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
-          <Option optionValue={""} optionLabel={"option3"} placeholder={"3rd Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
-          <Option optionValue={""} optionLabel={"option4"} placeholder={"4th Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
+    <div className='w-screen h-screen flex justify-around items-center'>
+      <section className='w w-2/5 bg-gray-200/70 py-3 mt-1 px-2.5 rounded-xl flex flex-col justify-between h-[32%] shadow-sm shadow-gray-600'>
+        <textarea className='w-full h-2/5  text-center placeholder:text-black bg-white/80 rounded-lg resize-none pt-[5%]' placeholder='question goes here...' />
+        <main className='grid grid-cols-2 justify-center items-center gap-y-2 gap-x-4 mx-2 '>
+          <Option optionValue={""} optionLabel={"1"} placeholder={"1st Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
+          <Option optionValue={""} optionLabel={"2"} placeholder={"2nd Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
+          <Option optionValue={""} optionLabel={"3"} placeholder={"3rd Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
+          <Option optionValue={""} optionLabel={"4"} placeholder={"4th Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
         </main>
+        <aside className='flex mx-2'>
+          <span className='py-1 px-5 bg-white/80 rounded-sm'>Correct Option: {correctOption}</span>
+          <button className='py-1 px-5 bg-white/80 ml-5 rounded-sm' onClick={clickHandler}>Submit</button>
+        </aside>
+      </section>
+      <section>
+        s
       </section>
     </div>
   )
