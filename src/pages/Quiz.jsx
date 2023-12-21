107a51ee57ec761338639f53a975b680a57a9977
Wadad Parker <wadadparker5@gmail.com>
2023-12-22 02:09:09 +0530

    feat: clear input fields on question entry

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 
-const Option=({optionValue,optionLabel,placeholder, correctOption, changeCorrectOption})=>
+const Option=({optionValue,optionLabel,placeholder, correctOption,onChangeHandler, changeCorrectOption})=>
 {
   return (
     <aside className='relative'>
@@ -10,7 +10,7 @@ const Option=({optionValue,optionLabel,placeholder, correctOption, changeCorrect
         <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
       </svg>
     </span>
-    <input className='text-center rounded-md p-3 bg-white/80 w-full' placeholder={placeholder}/>
+    <input className='text-center rounded-md p-3 bg-white/80 w-full' placeholder={placeholder} value={optionValue} onChange={(e)=>onChangeHandler(false,e.target.value,optionLabel)}/>
   </aside>
   )
 }
@@ -23,7 +23,7 @@ const Quiz = () => {
   // Maintain this below state in redux
   const [questions, setQuestions]= useState([]);
 
-  const changeCorrectOption = (selectedOption) => selectedOption!==correctOption?setCorrectOption(selectedOption):setCorrectOption("");
+  const changeCorrectOption = (selectedOption) => selectedOption!==correctOption && options[selectedOption]!==""?setCorrectOption(selectedOption):setCorrectOption("");
 
   const clickHandler=()=>
   {
@@ -36,22 +36,26 @@ const Quiz = () => {
       }))
     };
     setQuestions(prev=>[...prev,finalQuestion]);
-    
+    setQuestion("");
+    setOptions({ 1: "", 2: "", 3: "", 4: "" });
+    setCorrectOption("");
   }
+
+  const onChangeHandler=(isQuestion,text,optionNumber)=> isQuestion?setQuestion(text):setOptions(prev=>({...prev,[optionNumber]:text}))
 
   return (
     <div className='w-screen h-screen flex justify-around items-center'>
       <section className='w w-2/5 bg-gray-200/70 py-3 mt-1 px-2.5 rounded-xl flex flex-col justify-between h-[32%] shadow-sm shadow-gray-600'>
-        <textarea className='w-full h-2/5  text-center placeholder:text-black bg-white/80 rounded-lg resize-none pt-[5%]' placeholder='question goes here...' />
+        <textarea value={question} className='w-full h-2/5  text-center placeholder:text-black bg-white/80 rounded-lg resize-none pt-[5%]' placeholder='question goes here...' onChange={(e)=>onChangeHandler(true,e.target.value)}/>
         <main className='grid grid-cols-2 justify-center items-center gap-y-2 gap-x-4 mx-2 '>
-          <Option optionValue={""} optionLabel={"1"} placeholder={"1st Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
-          <Option optionValue={""} optionLabel={"2"} placeholder={"2nd Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
-          <Option optionValue={""} optionLabel={"3"} placeholder={"3rd Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
-          <Option optionValue={""} optionLabel={"4"} placeholder={"4th Option"} correctOption={correctOption} changeCorrectOption={changeCorrectOption} />
+          <Option optionValue={options[1]} optionLabel={"1"} placeholder={"1st Option"} correctOption={correctOption} onChangeHandler={onChangeHandler} changeCorrectOption={changeCorrectOption} />
+          <Option optionValue={options[2]} optionLabel={"2"} placeholder={"2nd Option"} correctOption={correctOption} onChangeHandler={onChangeHandler} changeCorrectOption={changeCorrectOption} />
+          <Option optionValue={options[3]} optionLabel={"3"} placeholder={"3rd Option"} correctOption={correctOption} onChangeHandler={onChangeHandler} changeCorrectOption={changeCorrectOption} />
+          <Option optionValue={options[4]} optionLabel={"4"} placeholder={"4th Option"} correctOption={correctOption} onChangeHandler={onChangeHandler} changeCorrectOption={changeCorrectOption} />
         </main>
         <aside className='flex mx-2'>
-          <span className='py-1 px-5 bg-white/80 rounded-sm'>Correct Option: {correctOption}</span>
-          <button className='py-1 px-5 bg-white/80 ml-5 rounded-sm' onClick={clickHandler}>Submit</button>
+          <span className='py-1 px-5 bg-white/80 rounded-sm  min-w-[30%]'>Correct Option: {correctOption!==""?correctOption:"-"}</span>
+          <button className='py-1 px-5 bg-white/80 ml-5 rounded-sm disabled:brightness-75 disabled:cursor-not-allowed' disabled={correctOption==="" || question===""} onClick={clickHandler}>Submit</button>
         </aside>
       </section>
       <section>
